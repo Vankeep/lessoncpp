336703b64d4b179b502ad7609caf97bbdb5ed1a0
Ivan Mashkov <mashkovvan93@yandex.ru>
2024-03-25 00:21:02 +0300

    Строки и указатели

--- lesson/63.cpp
+++ lesson/63.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#define NEW_SRT cout << endl
 using namespace std;
 
+/*
+Строки и указатели
+*/
+
+int srtLen(const char* str) {
+    int count = 0;
+    while (*str != '\0') {
+        count++;
+        str++;
+    }
+    return count;
+}
 
 int main() {
+     char str[] = "Hello";
+     char* foo = str;
+     cout << foo;          //Hello
 
+     NEW_SRT;
+     const char* srtArr2[] = { "Hello", "world", "!" };
+     for (int i = 0; i < 3; i++) {
+          cout << srtArr2[i] << " ";
+     }
+
+     NEW_SRT;
+     std::string srtArr3[] = { "Hello", "world", "!" };
+     for (const string& str : srtArr3) {
+          cout << str << " ";
+     }
+
+     NEW_SRT;
+     char srtArr4[][6] = { "Hello", "world", "!" };
+     for (const string& str : srtArr4) {
+          cout << str << " ";
+     }
+
+     NEW_SRT;
+     cout << srtLen(str) << endl;
+     
      return 0;
 }
