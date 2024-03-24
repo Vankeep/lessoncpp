84b182add27a89d8ecace500bf1d3f51426a3c78
Ivan Mashkov <mashkovvan93@yandex.ru>
2024-03-24 21:06:10 +0300

    push and clear back in array

--- lesson/pushBaskArray.cpp
+++ lesson/pushBaskArray.cpp
@@ -26,13 +26,33 @@ void pushBack(int*& arr, int& size, const int value) {
      arr = tmpArr;
 }
 
+void clearBack(int*& arr, int& size, const int count = 1) {
+     int* newArr = new int [size - count] {};
+     for (int i = 0; i < size - count; i++) {
+          newArr[i] = arr[i];
+     }
+     size = size - count;
+     delete[] arr;
+     arr = newArr;
+}
+
 int main() {
      int size = 10;
      int* arr = new int[size] {};
      fillArray(arr, size);
-     showArray(arr, size);    //&arr = 0x61ab7ff6a0		arr = 0x19e36d55460		*arr = 8		8 9 9 1 7 5 5 10 1 0 
+     showArray(arr, size);
      pushBack(arr, size, 10);
-     showArray(arr, size);    //&arr = 0x61ab7ff6a0		arr = 0x19e36d569f0		*arr = 8		8 9 9 1 7 5 5 10 1 0 10 
+     showArray(arr, size);
+     clearBack(arr, size, 2);
+     showArray(arr, size);
      delete[] arr;
      return 0;
 }
+// showArray()
+// &arr = 0xf296fffdd0		arr = 0x23b51465aa0		*arr = 8		8(0x23b51465aa0) 9(0x23b51465aa4) 9(0x23b51465aa8) 1(0x23b51465aac) 7(0x23b51465ab0) 5(0x23b51465ab4) 5(0x23b51465ab8) 10(0x23b51465abc) 1(0x23b51465ac0) 0(0x23b51465ac4) 
+
+// showArray()
+// &arr = 0xf296fffdd0		arr = 0x23b51466e50		*arr = 8		8(0x23b51466e50) 9(0x23b51466e54) 9(0x23b51466e58) 1(0x23b51466e5c) 7(0x23b51466e60) 5(0x23b51466e64) 5(0x23b51466e68) 10(0x23b51466e6c) 1(0x23b51466e70) 0(0x23b51466e74) 10(0x23b51466e78) 
+
+// showArray()
+// &arr = 0xf296fffdd0		arr = 0x23b51465920		*arr = 8		8(0x23b51465920) 9(0x23b51465924) 9(0x23b51465928) 1(0x23b5146592c) 7(0x23b51465930) 5(0x23b51465934) 5(0x23b51465938) 10(0x23b5146593c) 1(0x23b51465940) 
