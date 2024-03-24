da86d6cd91ebfa82717a3c03879b27724fb58d7c
Ivan Mashkov <mashkovvan93@yandex.ru>
2024-03-24 17:29:33 +0300

    Links

--- lesson/links.cpp
+++ lesson/links.cpp
@@ -7,7 +7,7 @@ int main() {
      int* pa = &a;
      int& aRef = a;
 
-     cout << "a = " << a << endl;                      //a = 	5
+     std::cout << "a = " << a << std::endl;            //a = 	5
      cout << "&a = " << &a << endl;                    //&a = 	0x42a87ffadc
      cout << "pa = " << pa << endl;                    //pa = 	0x42a87ffadc
      cout << "*pa = " << *pa << endl;                  //*pa = 	5
@@ -15,9 +15,9 @@ int main() {
      cout << "aRef = " << aRef << endl;                //aRef = 	5
      cout << "&aRef = " << &aRef << endl << endl;      //&aRef = 0x42a87ffadc
 
-     int *b = new int(7);
-     int *pb = b;
-     int &bRef = *b;
+     int* b = new int(7);
+     int* pb = b;
+     int& bRef = *b;
      cout << "b = " << b << endl;                      //b = 	0x1b06df99040
      cout << "&b = " << &b << endl;                    //&b = 	0x42a87ffac8
      cout << "*b = " << *b << endl;                    //*b = 	7
