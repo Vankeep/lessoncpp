0723d7f1f8eef25729d8c0cd66c817cc90ee806b
Ivan Mashkov <mashkovvan93@yandex.ru>
2024-03-24 03:41:30 +0300

    links

--- lesson/links.cpp
+++ lesson/links.cpp
@@ -7,24 +7,24 @@ int main() {
      int* pa = &a;
      int& aRef = a;
 
-     cout << "a = \t\t" << a << endl;                  //a = 	5
-     cout << "&a = \t" << &a << endl;                  //&a = 	0x42a87ffadc
-     cout << "pa = \t" << pa << endl;                  //pa = 	0x42a87ffadc
-     cout << "*pa = \t" << *pa << endl;                //*pa = 	5
-     cout << "&pa = \t" << &pa << endl;                //&pa = 	0x42a87ffad0
-     cout << "aRef = \t" << aRef << endl;              //aRef = 	5
-     cout << "&aRef = \t" << &aRef << endl << endl;    //&aRef = 0x42a87ffadc
+     cout << "a = " << a << endl;                      //a = 	5
+     cout << "&a = " << &a << endl;                    //&a = 	0x42a87ffadc
+     cout << "pa = " << pa << endl;                    //pa = 	0x42a87ffadc
+     cout << "*pa = " << *pa << endl;                  //*pa = 	5
+     cout << "&pa = " << &pa << endl;                  //&pa = 	0x42a87ffad0
+     cout << "aRef = " << aRef << endl;                //aRef = 	5
+     cout << "&aRef = " << &aRef << endl << endl;      //&aRef = 0x42a87ffadc
 
      int *b = new int(7);
      int *pb = b;
      int &bRef = *b;
-     cout << "b = \t\t" << b << endl;                  //b = 	0x1b06df99040
-     cout << "&b = \t" << &b << endl;                  //&b = 	0x42a87ffac8
-     cout << "*b = \t" << *b << endl;                  //*b = 	7
-     cout << "pb = \t" << pb << endl;                  //pb = 	0x1b06df99040
-     cout << "&pb = \t" << &pb << endl;                //&pb = 	0x42a87ffac0
-     cout << "*pb = \t" << *pb << endl;                //*pb = 	7
-     cout << "bRef = \t" << bRef << endl;              //bRef = 	7
-     cout << "&bRef = \t" << &bRef << endl;            //&bRef = 0x1b06df99040
+     cout << "b = " << b << endl;                      //b = 	0x1b06df99040
+     cout << "&b = " << &b << endl;                    //&b = 	0x42a87ffac8
+     cout << "*b = " << *b << endl;                    //*b = 	7
+     cout << "pb = " << pb << endl;                    //pb = 	0x1b06df99040
+     cout << "&pb = " << &pb << endl;                  //&pb = 	0x42a87ffac0
+     cout << "*pb = " << *pb << endl;                  //*pb = 	7
+     cout << "bRef = " << bRef << endl;                //bRef = 	7
+     cout << "&bRef = " << &bRef << endl;              //&bRef = 0x1b06df99040
      return 0;
 }
