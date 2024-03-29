9253969f0de8285e193114878dae816bed206a73
Ivan Mashkov <mashkovvan93@yandex.ru>
2024-03-29 21:11:00 +0300

    Перегрузка операторов ++ --

--- oop/86.cpp
+++ oop/86.cpp
@@ -5,7 +5,54 @@ using namespace std;
  Перегрузка операторов ++ --
 */
 
-int main() {
+class Point {
+     int x;
+     int y;
 
+public:
+     Point(int x, int y) {
+          this->x = x;
+          this->y = y;
+     }
+     // Префиксный инкремент
+     Point& operator++() {
+          ++x;
+          ++y;
+          return *this;
+     }
+
+     // Постфиксный инкремент
+     Point operator++(int) {
+          Point temp(*this);
+          this->x++;
+          this->y++;
+          return temp;
+     }
+      // Префиксный декримент
+     Point& operator--() {
+          --x;
+          --y;
+          return *this;
+     }
+
+     // Постфиксный декримент
+     Point operator--(int) {
+          Point temp(*this);
+          this->x--;
+          this->y--;
+          return temp;
+     }
+      // Перегрузка оператора <<
+     friend ostream& operator<<(ostream& os, const Point& p) {
+          os << "{" << p.x << ", " << p.y << "}";
+          return os;
+     }
+};
+
+int main() {
+     Point a(2, 3);
+     cout << a << endl;
+     cout << a++ << endl;
+     cout << --a << endl;
      return 0;
 }
